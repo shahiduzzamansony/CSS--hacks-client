4183c3ce35aa2ce34c4c782f12dcf15b04897d60
Your shahiduzzamansony <shahiduzzamansony2703@gmail.com>
2022-10-27 01:08:19 +0600

    google login implemented

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,11 +1,26 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged, signInWithPopup } from "firebase/auth";
+import app from "../../firebase/firebase.config";
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-  const user = { displayName: "ami" };
+  const [user, setUser] = useState(null);
 
-  const authInfo = { user };
+  const providerLogin = (provider) => {
+    return signInWithPopup(auth, provider);
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (newUser) => {
+      setUser(newUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const authInfo = { user, providerLogin };
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
--- src/pages/shared/Header/Header.js
+++ src/pages/shared/Header/Header.js
@@ -1,11 +1,27 @@
+import { GoogleAuthProvider } from "firebase/auth";
 import React, { useContext } from "react";
+import { Image } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { AuthContext } from "../../../contexts/AuthProvider/AuthProvider";
+import { FaJenkins } from "react-icons/fa";
 
 const Header = () => {
   const { user } = useContext(AuthContext);
+
+  const { providerLogin } = useContext(AuthContext);
+
+  const googleProvider = new GoogleAuthProvider();
+
+  const handleGoogleLogIn = () => {
+    providerLogin(googleProvider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => console.error(error));
+  };
 
   return (
     <div>
@@ -23,9 +39,22 @@ const Header = () => {
             </Nav>
             <Nav>
               <Nav.Link href="#register">Register</Nav.Link>
-              <Nav.Link href="#login">LogIn</Nav.Link>
+              <Nav.Link href="#login" onClick={handleGoogleLogIn}>
+                LogIn
+              </Nav.Link>
               <Nav.Link eventKey={2} href="#profile">
                 {user?.displayName}
+              </Nav.Link>
+              <Nav.Link eventKey={2} href="#profile">
+                {user?.photoURL ? (
+                  <Image
+                    style={{ height: "30px" }}
+                    roundedCircle
+                    src={user?.photoURL}
+                  ></Image>
+                ) : (
+                  <FaJenkins></FaJenkins>
+                )}
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>

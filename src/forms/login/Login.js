262ea14b3c4025a852d690e515516365b7146215
Your shahiduzzamansony <shahiduzzamansony2703@gmail.com>
2022-10-28 01:33:08 +0600

    api

--- src/forms/login/Login.js
+++ src/forms/login/Login.js
@@ -76,9 +76,6 @@ const Login = () => {
             required
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
         <Button variant="primary" type="submit">
           Submit
         </Button>

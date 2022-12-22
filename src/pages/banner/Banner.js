b862e63158a9a449591963d332981c27ad4b019f
Your shahiduzzamansony <shahiduzzamansony2703@gmail.com>
2022-12-22 13:45:24 +0600

    type

--- src/pages/banner/Banner.js
+++ src/pages/banner/Banner.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Banner.css";
+import Typewriter from "typewriter-effect";
 
 const Banner = () => {
-  return <div></div>;
+  return (
+    <div className="type ">
+      <Typewriter
+        onInit={(typewriter) => {
+          typewriter
+
+            .typeString("CSS Hacks")
+
+            .pauseFor(1000)
+            .deleteAll()
+            .typeString("Welcomes You")
+            .start();
+        }}
+      />
+    </div>
+  );
 };
 
 export default Banner;
